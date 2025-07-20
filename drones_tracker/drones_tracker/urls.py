ffedbb54a3474299b18c0971b29af4c422887fe7
Mahmoud Alabsi <medoabsi6@gmail.com>
2025-07-20 14:47:12 +0300

    Add swagger configs and urls.

--- drones_tracker/drones_tracker/urls.py
+++ drones_tracker/drones_tracker/urls.py
@@ -15,8 +15,29 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_yasg import openapi
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Drones Tracker API",
+        default_version='v1',
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('swagger/', schema_view.with_ui('swagger',
+         cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc',
+         cache_timeout=0), name='schema-redoc'),
+    path('swagger.json', schema_view.without_ui(
+        cache_timeout=0), name='schema-json'),
+
     path('admin/', admin.site.urls),
+    path('api/v1/', include('tracker.urls'))
 ]
